0d3727895333c32a206b4784926ed3b26d5e756a
HaeChan Jeong <108115672+harryputer1214@users.noreply.github.com>
2023-02-03 18:42:13 +0900

    Create w8_엄청 큰수 비교하기.cpp

---
+++ w8_엄청 큰수 비교하기.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <string>
+using namespace std;
+
+int main() {
+	string num1;
+	string num2;
+
+	cin>>num1>>num2;
+	if(num1<num2==1){
+		cout<<num2;
+	}
+	else{
+		cout<<num1;
+	}
+}
+
+/*
+10000000000000
+1000000000000
+
+*/
