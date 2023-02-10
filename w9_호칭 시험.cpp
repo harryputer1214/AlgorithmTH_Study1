dc08d3c1da464460bc19da23480c83878eaa1219
HaeChan Jeong <108115672+harryputer1214@users.noreply.github.com>
2023-02-10 18:14:00 +0900

    Create w9_호칭 시험.cpp

---
+++ w9_호칭 시험.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+int main() {
+  string mine,your;
+	cin>>mine;
+	cin>>your;
+
+	if(mine>your){
+		printf("sir");
+	}
+	else{
+		printf("kid");
+	}
+}
+
+/*
+05D517719F031
+850B55DB369AB
+
+05DC17719F013
+05D755DB369AB
+
+*/
